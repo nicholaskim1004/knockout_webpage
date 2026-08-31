dbab4bc91cb774c8c539911afeb7a6e19436b3c3
nicholaskim1004 <nicholaskim.1010@gmail.com>
2026-08-30 23:16:01 -0400

    added basketballs

--- knockout/src/App.jsx
+++ knockout/src/App.jsx
@@ -33,7 +33,7 @@ function App() {
 
     return (
         <div>
-            <h1>Fraction Knockout</h1>
+            <h1>🏀Fraction Knockout🏀</h1>
             <div className="dropdown">
                 <button onClick={() => setIsOpen(!isopen)}>
                     Difficulty: {difficulty}
